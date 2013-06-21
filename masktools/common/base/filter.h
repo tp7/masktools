tp7/masktools

commit 5ec787d0a5c5f148fb9473c2a5112641a5a08b24

Remove two levels of useless inheritance
Or was it one layer? It was kinda hard to wrap my head around it.

masktools/common/base/filter.h
@@ -2,7 +2,7 @@
 #define __Mt_Base_H__
 
 #include "../../../common/functions/functions.h"
-#include "../../../common/filter/filter.h"
+#include "../../../common/params/params.h"
 #include "../../../common/parser/parser.h"
 #include "../../../common/utils/utils.h"
 #include "../../common/params/params.h"
@@ -10,23 +10,36 @@
 
 namespace Filtering { namespace MaskTools { 
 
-template<class ParentFilter>
-class Filter : public ParentFilter {
+enum class FilterProcessingType {
+    INPLACE,
+    CHILD
+};
+
+class Filter {
 
 protected:
+    ClipArray childs;
+    Parameters parameters;
 
-   Operator operators[3];
+    int nWidth, nHeight;
+    Colorspace C;
 
-   int nXOffset, nYOffset, nXOffsetUV, nYOffsetUV;
-   int nCoreWidth, nCoreHeight, nCoreWidthUV, nCoreHeightUV;
+    String error;
+    CpuFlags flags;
+    Operator operators[3];
 
-   std::vector<Frame<const Byte> > frames;
-   Constraint constraints[3];
+    bool inPlace_;
+    int nXOffset, nYOffset, nXOffsetUV, nYOffsetUV;
+    int nCoreWidth, nCoreHeight, nCoreWidthUV, nCoreHeightUV;
+
+    std::vector<Frame<const Byte> > frames;
+    Constraint constraints[3];
 
 private:
 
-   ProcessorList<Functions::Memset>  memsets;
-   ProcessorList<Functions::Copy>    copies;
+    ProcessorList<Functions::Memset>  memsets;
+    ProcessorList<Functions::Copy>    copies;
+
 
 protected:
 
@@ -57,162 +70,180 @@ protected:
 
 public:
 
-   Filter(const Parameters &parameters) :
-      ParentFilter( parameters ),
-      nXOffset( parameters["offx"] ),
-      nYOffset( parameters["offy"] ),
-      nCoreWidth( parameters["w"] ),
-      nCoreHeight( parameters["h"] )
-   {
-      nXOffset = parameters["offx"];
-      nYOffset = parameters["offy"];
-      nCoreWidth = parameters["w"];
-      nCoreHeight = parameters["h"];
-
-      operators[0] = Operator( parameters["Y"] );
-      operators[1] = Operator( parameters["U"] );
-      operators[2] = Operator( parameters["V"] );
-
-      if ( nXOffset < 0 || nXOffset > nWidth  ) nXOffset = 0;
-      if ( nYOffset < 0 || nYOffset > nHeight ) nYOffset = 0;
-      if ( nXOffset + nCoreWidth  > nWidth  || nCoreWidth  < 0 ) nCoreWidth  = nWidth - nXOffset;
-      if ( nYOffset + nCoreHeight > nHeight || nCoreHeight < 0 ) nCoreHeight = nHeight - nYOffset;
-
-      if ( parameters["chroma"].is_defined() )
-      {
-         /* overrides chroma channel operators according to the "chroma" string */
-         String chroma = parameters["chroma"].toString();
-
-         if ( chroma == "process" )
-            operators[1] = operators[2] = PROCESS;
-         else if ( chroma == "copy" )
-            operators[1] = operators[2] = COPY;
-         else if ( chroma == "copy first" )
-            operators[1] = operators[2] = COPY;
-         else if ( chroma == "copy second" )
-            operators[1] = operators[2] = COPY_SECOND;
-         else if ( chroma == "copy third" )
-            operators[1] = operators[2] = COPY_THIRD;
-         else
-            operators[1] = operators[2] = Operator( MEMSET, atoi( chroma.c_str() ) );
-      }
-
-      /* checks the operators */
-      for ( int i = 0; i < 3; i++ )
-      {
-         if ( operators[i] == COPY_THIRD && childs.size() < 3 )
-            operators[i] = COPY_SECOND;
-         if ( operators[i] == COPY_SECOND && childs.size() < 2 )
-            operators[i] = COPY;
-      }
-
-      if ( is_in_place() )
-      {
-         /* in place filters copy differently */
-         for ( int i = 0; i < 3; i++ )
-         {
-            switch ( operators[i].getMode() )
-            {
-            case COPY: operators[i] = NONE; break;
-            case COPY_SECOND: operators[i] = COPY; break;
-            case COPY_THIRD: operators[i] = COPY_SECOND; break;
+    Filter(const Parameters &parameters, FilterProcessingType processingType) :
+        inPlace_(processingType == FilterProcessingType::INPLACE),
+        nXOffset( parameters["offx"] ),
+        nYOffset( parameters["offy"] ),
+        nCoreWidth( parameters["w"] ),
+        nCoreHeight( parameters["h"] )
+    {
+        for(auto &param: parameters) {
+            if (param.getType() == TYPE_CLIP) {
+                childs.push_back(param.getValue());
             }
-         }
-      }
+        }
 
-      /* effective modes */
-      print( LOG_DEBUG, "modes : %i %i %i\n", operators[0].getMode(), operators[1].getMode(), operators[2].getMode() );
+        assert( !childs.empty() );
 
-      /* cpu flags */
-      if ( !parameters["mmx"].toBool() ) flags &= ~CPU_MMX;
-      if ( !parameters["isse"].toBool() ) flags &= ~CPU_ISSE;
-      if ( !parameters["sse2"].toBool() ) flags &= ~CPU_SSE2;
-      if ( !parameters["sse3"].toBool() ) flags &= ~CPU_SSE3;
-      if ( !parameters["d3now"].toBool() ) flags &= ~CPU_3DNOW;
-      if ( !parameters["d3now2"].toBool() ) flags &= ~CPU_3DNOW2;
-      if ( !parameters["ssse3"].toBool() ) flags &= ~CPU_SSSE3;
-      if ( !parameters["sse4"].toBool() ) flags &= ~CPU_SSE4;
+        nWidth = childs[0]->width();
+        nHeight = childs[0]->height();
+        C = childs[0]->colorspace();
 
-      print( LOG_DEBUG, "using cpu flags : 0x%x\n", flags );
+        nXOffset = parameters["offx"];
+        nYOffset = parameters["offy"];
+        nCoreWidth = parameters["w"];
+        nCoreHeight = parameters["h"];
 
-      /* chroma offsets and box */
-      if ( C != COLORSPACE_Y8 && C != COLORSPACE_NONE )
-      {
-         nXOffsetUV = nXOffset / width_ratios[1][C];
-         nYOffsetUV = nYOffset / height_ratios[1][C];
-         nCoreWidthUV = (nXOffset + nCoreWidth) / width_ratios[1][C] - nXOffsetUV;
-         nCoreHeightUV = (nYOffset + nCoreHeight) / height_ratios[1][C] - nYOffsetUV;
-      }
+        operators[0] = Operator( parameters["Y"] );
+        operators[1] = Operator( parameters["U"] );
+        operators[2] = Operator( parameters["V"] );
 
-      /* effective offset */
-      print( LOG_DEBUG, "offset : %i %i, width x height : %i x %i\n", nXOffset, nYOffset, nCoreWidth, nCoreHeight );
+        if ( nXOffset < 0 || nXOffset > nWidth  ) nXOffset = 0;
+        if ( nYOffset < 0 || nYOffset > nHeight ) nYOffset = 0;
+        if ( nXOffset + nCoreWidth  > nWidth  || nCoreWidth  < 0 ) nCoreWidth  = nWidth - nXOffset;
+        if ( nYOffset + nCoreHeight > nHeight || nCoreHeight < 0 ) nCoreHeight = nHeight - nYOffset;
 
-      /* default processors */
-      /* memsets */
-      memsets.push_back(Processor<Functions::Memset>(&Functions::memset_c, Constraint(CPU_NONE, 1, 1, 1, 1), 0));
-      memsets.push_back(Processor<Functions::Memset>(&memset8_mmx, Constraint(CPU_MMX, 8, 1, 1, 1), 1));
-      memsets.push_back(Processor<Functions::Memset>(&memset8_isse, Constraint(CPU_ISSE, 8, 1, 1, 1), 2));
-      memsets.push_back(Processor<Functions::Memset>(&memset8_3dnow, Constraint(CPU_3DNOW, 8, 1, 1, 1), 3));
+        if ( parameters["chroma"].is_defined() )
+        {
+            /* overrides chroma channel operators according to the "chroma" string */
+            String chroma = parameters["chroma"].toString();
 
-      /* copy */
-      copies.push_back(Processor<Functions::Copy>(&Functions::copy_c, Constraint(CPU_NONE, 1, 1, 1, 1), 0));
-      copies.push_back(Processor<Functions::Copy>(&copy8_mmx, Constraint(CPU_MMX, 8, 1, 1, 1), 1));
-      copies.push_back(Processor<Functions::Copy>(&copy8_isse, Constraint(CPU_ISSE, 8, 1, 1, 1), 2));
-      copies.push_back(Processor<Functions::Copy>(&copy8_3dnow, Constraint(CPU_3DNOW, 8, 1, 1, 1), 3));
+            if ( chroma == "process" )
+                operators[1] = operators[2] = PROCESS;
+            else if ( chroma == "copy" )
+                operators[1] = operators[2] = COPY;
+            else if ( chroma == "copy first" )
+                operators[1] = operators[2] = COPY;
+            else if ( chroma == "copy second" )
+                operators[1] = operators[2] = COPY_SECOND;
+            else if ( chroma == "copy third" )
+                operators[1] = operators[2] = COPY_THIRD;
+            else
+                operators[1] = operators[2] = Operator( MEMSET, atoi( chroma.c_str() ) );
+        }
 
-      /* check the colorspace */
-      if ( C == COLORSPACE_NONE )
-         error = "unsupported colorspace. masktools only support planar YUV colorspaces (YV12, YV16, YV24)";
-   }
+        /* checks the operators */
+        for ( int i = 0; i < 3; i++ )
+        {
+            if ( operators[i] == COPY_THIRD && childs.size() < 3 )
+                operators[i] = COPY_SECOND;
+            if ( operators[i] == COPY_SECOND && childs.size() < 2 )
+                operators[i] = COPY;
+        }
 
-   void process_plane(int n, const Plane<Byte> &output_plane, int nPlane)
-   {
-      /* need multiple constraints */
-      Constraint constraint = constraints[nPlane];
+        if ( is_in_place() )
+        {
+            /* in place filters copy differently */
+            for ( int i = 0; i < 3; i++ )
+            {
+                switch ( operators[i].getMode() )
+                {
+                case COPY: operators[i] = NONE; break;
+                case COPY_SECOND: operators[i] = COPY; break;
+                case COPY_THIRD: operators[i] = COPY_SECOND; break;
+                }
+            }
+        }
 
-      switch (operators[nPlane].getMode())
-      {
-      case COPY: copies.best_processor(constraint)( output_plane, output_plane.pitch(),
-                                                    frames[0].plane(nPlane), frames[0].plane(nPlane).pitch(),
-                                                    output_plane.width(), output_plane.height() ); break;
-      case COPY_SECOND: copies.best_processor(constraint)( output_plane, output_plane.pitch(),
-                                                           frames[1].plane(nPlane), frames[1].plane(nPlane).pitch(),
-                                                           output_plane.width(), output_plane.height() ); break;
-      case COPY_THIRD: copies.best_processor(constraint)( output_plane, output_plane.pitch(),
-                                                          frames[2].plane(nPlane), frames[2].plane(nPlane).pitch(),
-                                                          output_plane.width(), output_plane.height() ); break;
-      case MEMSET: memsets.best_processor(constraint)( output_plane, output_plane.pitch(), output_plane.width(), output_plane.height(), static_cast<Byte>(operators[nPlane].value()) ); break;
-      case PROCESS: process( n, output_plane, nPlane ); break;
-      case NONE: 
-      default: break;
-      }
-   }
+        /* effective modes */
+        print( LOG_DEBUG, "modes : %i %i %i\n", operators[0].getMode(), operators[1].getMode(), operators[2].getMode() );
 
-   virtual Frame<Byte> get_frame(int n, const Frame<Byte> &output_frame)
-   {
-      Functions::Asm asmLock;
+        /* cpu flags */
+        if ( !parameters["mmx"].toBool() ) flags &= ~CPU_MMX;
+        if ( !parameters["isse"].toBool() ) flags &= ~CPU_ISSE;
+        if ( !parameters["sse2"].toBool() ) flags &= ~CPU_SSE2;
+        if ( !parameters["sse3"].toBool() ) flags &= ~CPU_SSE3;
+        if ( !parameters["d3now"].toBool() ) flags &= ~CPU_3DNOW;
+        if ( !parameters["d3now2"].toBool() ) flags &= ~CPU_3DNOW2;
+        if ( !parameters["ssse3"].toBool() ) flags &= ~CPU_SSSE3;
+        if ( !parameters["sse4"].toBool() ) flags &= ~CPU_SSE4;
 
-      /* TODO : correct SSE2 alignement check for multiclip functions, once there is SSE2 functions */
+        print( LOG_DEBUG, "using cpu flags : 0x%x\n", flags );
 
-      Frame<Byte> output = output_frame.offset( nXOffset, nYOffset, nCoreWidth, nCoreHeight );
+        /* chroma offsets and box */
+        if ( C != COLORSPACE_Y8 && C != COLORSPACE_NONE )
+        {
+            nXOffsetUV = nXOffset / width_ratios[1][C];
+            nYOffsetUV = nYOffset / height_ratios[1][C];
+            nCoreWidthUV = (nXOffset + nCoreWidth) / width_ratios[1][C] - nXOffsetUV;
+            nCoreHeightUV = (nYOffset + nCoreHeight) / height_ratios[1][C] - nYOffsetUV;
+        }
 
-      for ( int i = 0; i < int( input_configuration().size() ); i++ )
-         frames.push_back( childs[ input_configuration()[i].index() ]->get_const_frame( n + input_configuration()[i].offset() ).offset( nXOffset, nYOffset, nCoreWidth, nCoreHeight ) );
+        /* effective offset */
+        print( LOG_DEBUG, "offset : %i %i, width x height : %i x %i\n", nXOffset, nYOffset, nCoreWidth, nCoreHeight );
 
-      for ( int i = 0; i < plane_counts[C]; i++ )
-         constraints[i] = Constraint( flags, output.plane(i) );
+        /* default processors */
+        /* memsets */
+        memsets.push_back(Processor<Functions::Memset>(&Functions::memset_c, Constraint(CPU_NONE, 1, 1, 1, 1), 0));
+        memsets.push_back(Processor<Functions::Memset>(&memset8_mmx, Constraint(CPU_MMX, 8, 1, 1, 1), 1));
+        memsets.push_back(Processor<Functions::Memset>(&memset8_isse, Constraint(CPU_ISSE, 8, 1, 1, 1), 2));
+        memsets.push_back(Processor<Functions::Memset>(&memset8_3dnow, Constraint(CPU_3DNOW, 8, 1, 1, 1), 3));
 
-      for ( int i = 0; i < int(frames.size()); i++ )
-         for ( int j = 0; j < plane_counts[frames[i].colorspace()]; j++ )
-            constraints[j] = Constraint( constraints[j], frames[i].plane(j) );
+        /* copy */
+        copies.push_back(Processor<Functions::Copy>(&Functions::copy_c, Constraint(CPU_NONE, 1, 1, 1, 1), 0));
+        copies.push_back(Processor<Functions::Copy>(&copy8_mmx, Constraint(CPU_MMX, 8, 1, 1, 1), 1));
+        copies.push_back(Processor<Functions::Copy>(&copy8_isse, Constraint(CPU_ISSE, 8, 1, 1, 1), 2));
+        copies.push_back(Processor<Functions::Copy>(&copy8_3dnow, Constraint(CPU_3DNOW, 8, 1, 1, 1), 3));
 
-      for ( int i = 0; i < plane_counts[C]; i++ )
-         process_plane( n, output.plane(i), i );
+        /* check the colorspace */
+        if ( C == COLORSPACE_NONE )
+            error = "unsupported colorspace. masktools only support planar YUV colorspaces (YV12, YV16, YV24)";
+    }
 
-      frames.clear();
+    void process_plane(int n, const Plane<Byte> &output_plane, int nPlane)
+    {
+        /* need multiple constraints */
+        Constraint constraint = constraints[nPlane];
 
-      return output_frame;
-   }
+        switch (operators[nPlane].getMode())
+        {
+        case COPY: copies.best_processor(constraint)( output_plane, output_plane.pitch(),
+                       frames[0].plane(nPlane), frames[0].plane(nPlane).pitch(),
+                       output_plane.width(), output_plane.height() ); break;
+        case COPY_SECOND: copies.best_processor(constraint)( output_plane, output_plane.pitch(),
+                              frames[1].plane(nPlane), frames[1].plane(nPlane).pitch(),
+                              output_plane.width(), output_plane.height() ); break;
+        case COPY_THIRD: copies.best_processor(constraint)( output_plane, output_plane.pitch(),
+                             frames[2].plane(nPlane), frames[2].plane(nPlane).pitch(),
+                             output_plane.width(), output_plane.height() ); break;
+        case MEMSET: memsets.best_processor(constraint)( output_plane, output_plane.pitch(), output_plane.width(), output_plane.height(), static_cast<Byte>(operators[nPlane].value()) ); break;
+        case PROCESS: process( n, output_plane, nPlane ); break;
+        case NONE: 
+        default: break;
+        }
+    }
+
+    virtual Frame<Byte> get_frame(int n, const Frame<Byte> &output_frame)
+    {
+        Functions::Asm asmLock;
+
+        /* TODO : correct SSE2 alignement check for multiclip functions, once there is SSE2 functions */
+
+        Frame<Byte> output = output_frame.offset( nXOffset, nYOffset, nCoreWidth, nCoreHeight );
+
+        for ( int i = 0; i < int( input_configuration().size() ); i++ )
+            frames.push_back( childs[ input_configuration()[i].index() ]->get_const_frame( n + input_configuration()[i].offset() ).offset( nXOffset, nYOffset, nCoreWidth, nCoreHeight ) );
+
+        for ( int i = 0; i < plane_counts[C]; i++ )
+            constraints[i] = Constraint( flags, output.plane(i) );
+
+        for ( int i = 0; i < int(frames.size()); i++ )
+            for ( int j = 0; j < plane_counts[frames[i].colorspace()]; j++ )
+                constraints[j] = Constraint( constraints[j], frames[i].plane(j) );
+
+        for ( int i = 0; i < plane_counts[C]; i++ )
+            process_plane( n, output.plane(i), i );
+
+        frames.clear();
+
+        return output_frame;
+    }
+
+    ClipArray &get_childs() { return childs; }
+
+    String get_error() const { return error; }
+    bool is_error() const { return !error.empty(); }
+    bool is_in_place() const { return inPlace_; }
 
 };
 

avs2x/filter.h
@@ -30,9 +30,9 @@ public:
    }
    PVideoFrame __stdcall GetFrame(int n, IScriptEnvironment *env)
    {
-      PVideoFrame dst = T::is_in_place() ? child->GetFrame(n, env) : env->NewVideoFrame(vi);
+      PVideoFrame dst = _filter.is_in_place() ? child->GetFrame(n, env) : env->NewVideoFrame(vi);
 
-      if ( T::is_in_place() )
+      if ( _filter.is_in_place() )
          env->MakeWritable( &dst );
 
       for ( int i = 0; i < int(_filter.get_childs().size()); i++ )
@@ -54,12 +54,5 @@ public:
 };
 
 } } // namespace Avisynth2x, Filtering
-
-#define AVS2X_DEFINE_FILTER(filter, blabla) \
-extern "C" __declspec(dllexport) const char* __stdcall AvisynthPluginInit2(IScriptEnvironment* env) \
-{ \
-   Filtering::Avisynth2x::Filter<filter>::create( env ); \
-   return ( blabla ); \
-}
 
 #endif

common/filter/filter.h
@@ -1,65 +1,0 @@
-#ifndef __Common_Filter_H__
-#define __Common_Filter_H__
-
-#include "../params/params.h"
-
-namespace Filtering {
-
-class Filter {
-
-protected:
-
-   ClipArray childs;
-   Parameters parameters;
-
-   int nWidth, nHeight;
-   Colorspace C;
-
-   String error;
-   CpuFlags flags;
-
-public:
-
-    Filter(const Parameters &parameters) : parameters(parameters), flags(Functions::get_cpu_flags())
-    {
-        for(auto &param: parameters) {
-            if (param.getType() == TYPE_CLIP) {
-                childs.push_back(param.getValue());
-            }
-        }
-
-        assert( !childs.empty() );
-
-        nWidth = childs[0]->width();
-        nHeight = childs[0]->height();
-        C = childs[0]->colorspace();
-    }
-
-    ClipArray &get_childs() { return childs; }
-
-    virtual Frame<Byte> get_frame(int n, const Frame<Byte> &output_frame) = 0;
-
-    String get_error() const { return error; }
-    bool is_error() const { return !error.empty(); }
-
-};
-
-class InPlaceFilter : public Filter {
-
-public:
-   InPlaceFilter(const Parameters &parameters) : Filter(parameters) { }
-
-   static bool is_in_place() { return true; }
-};
-
-class ChildFilter : public Filter {
-
-public:
-   ChildFilter(const Parameters &parameters) : Filter(parameters) {}
-
-   static bool is_in_place() { return false; }
-};
-
-} // namespace Filtering
-
-#endif

common/build/common.vcxproj
@@ -445,7 +445,6 @@
     <ClInclude Include="..\utils\utils.h" />
     <ClInclude Include="..\parser\parser.h" />
     <ClInclude Include="..\parser\symbol.h" />
-    <ClInclude Include="..\filter\filter.h" />
     <ClInclude Include="..\functions\functions.h" />
     <ClInclude Include="..\clip\clip.h" />
     <ClInclude Include="..\params\params.h" />

common/build/common.vcxproj.filters
@@ -7,9 +7,6 @@
     <Filter Include="parser">
       <UniqueIdentifier>{f3d27c6a-d827-4f58-9221-7c5a6c2e0cc1}</UniqueIdentifier>
     </Filter>
-    <Filter Include="filter">
-      <UniqueIdentifier>{1b95802a-9d6d-460c-b099-623b027e38c3}</UniqueIdentifier>
-    </Filter>
     <Filter Include="functions">
       <UniqueIdentifier>{308ba486-b70e-420f-ac4d-f7e43df52b24}</UniqueIdentifier>
     </Filter>
@@ -44,9 +41,6 @@
     </ClInclude>
     <ClInclude Include="..\parser\symbol.h">
       <Filter>parser</Filter>
-    </ClInclude>
-    <ClInclude Include="..\filter\filter.h">
-      <Filter>filter</Filter>
     </ClInclude>
     <ClInclude Include="..\functions\functions.h">
       <Filter>functions</Filter>

masktools/filters/binarize/binarize.h
@@ -35,7 +35,7 @@ DEFINE_PROCESSOR(binarize_255_t);
 #undef DEFINE_PROCESSOR_NO_MMX
 #undef DEFINE_PROCESSOR
 
-class Binarize : public MaskTools::Filter<InPlaceFilter>
+class Binarize : public MaskTools::Filter
 {
    Byte nThreshold;
    ProcessorList<Processor> processors;
@@ -48,7 +48,7 @@ protected:
    }
 
 public:
-   Binarize(const Parameters &parameters) : MaskTools::Filter<InPlaceFilter>(parameters)
+   Binarize(const Parameters &parameters) : MaskTools::Filter(parameters, FilterProcessingType::INPLACE)
    {
       nThreshold = convert<Byte, int>( parameters["threshold"].toInt() );
 

masktools/filters/blur/mappedblur.h
@@ -11,7 +11,7 @@ typedef void(Processor)(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdi
 extern Processor *mapped_below_c;
 extern Processor *mapped_all_c;
 
-class MappedBlur : public MaskTools::Filter<ChildFilter>
+class MappedBlur : public MaskTools::Filter
 {
    Short matrix[10];
 
@@ -24,7 +24,7 @@ protected:
    }
 
 public:
-   MappedBlur(const Parameters &parameters) : MaskTools::Filter<ChildFilter>( parameters )
+   MappedBlur(const Parameters &parameters) : MaskTools::Filter( parameters, FilterProcessingType::CHILD )
    {
       std::list<Parser::Symbol> coeffs = Parser::getDefaultParser().parse(parameters["kernel"].toString(), " ").getExpression();
       memset(matrix, 0, sizeof(matrix));

masktools/filters/convolution/convolution.h
@@ -12,7 +12,7 @@ extern Processor *convolution_i_s_c;
 extern Processor *convolution_f_m_c;
 extern Processor *convolution_i_m_c;
 
-class Convolution : public MaskTools::Filter<ChildFilter>
+class Convolution : public MaskTools::Filter
 {
    int *i_horizontal, *i_vertical;
    float *f_horizontal, *f_vertical;
@@ -34,7 +34,7 @@ protected:
    }
 
 public:
-   Convolution(const Parameters &parameters) : MaskTools::Filter<ChildFilter>( parameters )
+   Convolution(const Parameters &parameters) : MaskTools::Filter( parameters, FilterProcessingType::CHILD )
    {
       i_vertical = i_horizontal = NULL;
       f_vertical = f_horizontal = NULL;

commit 5756f7124a930f96c62bf9b89b3744d82f90af32

Fix broken asm detection
Regression in 5ec787d0a5c5f148fb9473c2a5112641a5a08b24

masktools/common/base/filter.h
@@ -75,7 +75,9 @@ public:
         nXOffset( parameters["offx"] ),
         nYOffset( parameters["offy"] ),
         nCoreWidth( parameters["w"] ),
-        nCoreHeight( parameters["h"] )
+        nCoreHeight( parameters["h"] ),
+        parameters(parameters), 
+        flags(Functions::get_cpu_flags())
     {
         for(auto &param: parameters) {
             if (param.getType() == TYPE_CLIP) {

masktools/common/params/params.h
@@ -42,7 +42,7 @@ public:
    bool operator!=(const Operator &operation) const { return mode != operation.mode; }
    bool operator==(Mode mode) const { return mode == this->mode; }
    bool operator!=(Mode mode) const { return mode != this->mode; }
-   Mode getMode() { return mode; }
+   Mode getMode() const { return mode; }
    int value() const { return nValue; }
 };
 