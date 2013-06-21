087a404b8371ff64e59d57d5592641827bb9423e
tp7 <tp7ptr@gmail.com>
2013-06-22 00:34:38 +1100

    Rewrite mt_binarize asm

--- masktools/filters/binarize/binarize.cpp
+++ masktools/filters/binarize/binarize.cpp
@@ -1,4 +1,5 @@
 #include "binarize.h"
+#include "../../common/simd.h"
 
 using namespace Filtering;
 
@@ -29,6 +30,88 @@ void binarize_t(Byte *pDst, ptrdiff_t nDstPitch, Byte nThreshold, int nWidth, in
          pDst[i] = op(pDst[i], nThreshold);
 }
 
+/* SSE2 functions */
+static inline __m128i upper_sse2_op(__m128i x, __m128i t, __m128i) {
+    auto r = _mm_subs_epu8(x, t);
+    return _mm_cmpeq_epi8(r, _mm_setzero_si128());
+}
+
+static inline __m128i lower_sse2_op(__m128i x, __m128i, __m128i t128) {
+    auto r = _mm_add_epi8(x, _mm_set1_epi32(0x80808080));
+    return _mm_cmpgt_epi8(r, t128);
+}
+
+static inline __m128i binarize_0_x_sse2_op(__m128i x, __m128i t, __m128i t128) {
+    auto upper = upper_sse2_op(x, t, t128);
+    return _mm_and_si128(upper, x);
+}
+
+static inline __m128i binarize_t_x_sse2_op(__m128i x, __m128i t, __m128i) {
+    return _mm_min_epu8(t, x);
+}
+
+static inline __m128i binarize_x_0_sse2_op(__m128i x, __m128i t, __m128i t128) {
+    auto lower = lower_sse2_op(x, t, t128);
+    return _mm_and_si128(lower, x);
+}
+
+static inline __m128i binarize_x_t_sse2_op(__m128i x, __m128i t, __m128i) {
+    return _mm_max_epu8(t, x);
+}
+
+static inline __m128i binarize_t_0_sse2_op(__m128i x, __m128i t, __m128i t128) {
+    auto lower = lower_sse2_op(x, t, t128);
+    return _mm_and_si128(lower, t);
+}
+
+static inline __m128i binarize_0_t_sse2_op(__m128i x, __m128i t, __m128i t128) {
+    auto upper = upper_sse2_op(x, t, t128);
+    return _mm_and_si128(upper, t);
+}
+
+static inline __m128i binarize_x_255_sse2_op(__m128i x, __m128i t, __m128i t128) {
+    auto upper = upper_sse2_op(x, t, t128);
+    return _mm_or_si128(upper, x);
+}
+
+static inline __m128i binarize_255_x_sse2_op(__m128i x, __m128i t, __m128i t128) {
+    auto lower = lower_sse2_op(x, t, t128);
+    return _mm_or_si128(lower, x);
+}
+
+static inline __m128i binarize_t_255_sse2_op(__m128i x, __m128i t, __m128i t128) {
+    auto upper = upper_sse2_op(x, t, t128);
+    return _mm_or_si128(upper, t);
+}
+
+static inline __m128i binarize_255_t_sse2_op(__m128i x, __m128i t, __m128i t128) {
+    auto lower = lower_sse2_op(x, t, t128);
+    return _mm_or_si128(lower, t);
+}
+
+template<decltype(simd_load_epi128) load, decltype(simd_store_epi128) store, 
+    decltype(upper_sse2_op) op, decltype(upper) op_c>
+void binarize_sse2_t(Byte *pDst, ptrdiff_t nDstPitch, Byte nThreshold, int nWidth, int nHeight)
+{
+    auto t = _mm_set1_epi32((nThreshold << 24) | (nThreshold << 16) | (nThreshold << 8) | nThreshold);
+    auto t128 = _mm_add_epi8(t, _mm_set1_epi32(0x80808080));
+    int wMod16 = (nWidth * 16) / 16;
+    auto pDst2 = pDst;
+
+    for ( int j = 0; j < nHeight; ++j ) {
+        for ( int i = 0; i < wMod16; i+=16 ) {
+            auto src = load(reinterpret_cast<const __m128i*>(pDst+i));
+            auto result = op(src, t, t128);
+            store(reinterpret_cast<__m128i*>(pDst+i), result);
+        }
+        pDst += nDstPitch;
+    }
+
+    if (nWidth > wMod16) {
+        binarize_t<op_c>(pDst2 + (nWidth - wMod16), nDstPitch, nThreshold, nWidth - wMod16, nHeight);
+    }
+}
+
 namespace Filtering { namespace MaskTools { namespace Filters { namespace Binarize {
 
 Processor *upper_c = &binarize_t<upper>;
@@ -43,5 +126,34 @@ Processor *binarize_x_255_c = &binarize_t<binarize_x_255>;
 Processor *binarize_t_255_c = &binarize_t<binarize_t_255>;
 Processor *binarize_255_x_c = &binarize_t<binarize_255_x>;
 Processor *binarize_255_t_c = &binarize_t<binarize_255_t>;
+
+
+Processor *upper_sse2 = &binarize_sse2_t<simd_loadu_epi128, simd_storeu_epi128, upper_sse2_op, upper>;
+Processor *lower_sse2 = &binarize_sse2_t<simd_loadu_epi128, simd_storeu_epi128, lower_sse2_op, lower>;
+Processor *binarize_0_x_sse2 = &binarize_sse2_t<simd_loadu_epi128, simd_storeu_epi128, binarize_0_x_sse2_op, binarize_0_x>;
+Processor *binarize_t_x_sse2 = &binarize_sse2_t<simd_loadu_epi128, simd_storeu_epi128, binarize_t_x_sse2_op, binarize_t_x>;
+Processor *binarize_x_0_sse2 = &binarize_sse2_t<simd_loadu_epi128, simd_storeu_epi128, binarize_x_0_sse2_op, binarize_x_0>;
+Processor *binarize_x_t_sse2 = &binarize_sse2_t<simd_loadu_epi128, simd_storeu_epi128, binarize_x_t_sse2_op, binarize_x_t>;
+Processor *binarize_t_0_sse2 = &binarize_sse2_t<simd_loadu_epi128, simd_storeu_epi128, binarize_t_0_sse2_op, binarize_t_0>;
+Processor *binarize_0_t_sse2 = &binarize_sse2_t<simd_loadu_epi128, simd_storeu_epi128, binarize_0_t_sse2_op, binarize_0_t>;
+Processor *binarize_x_255_sse2 = &binarize_sse2_t<simd_loadu_epi128, simd_storeu_epi128, binarize_x_255_sse2_op, binarize_x_255>;
+Processor *binarize_t_255_sse2 = &binarize_sse2_t<simd_loadu_epi128, simd_storeu_epi128, binarize_t_255_sse2_op, binarize_t_255>;
+Processor *binarize_255_x_sse2 = &binarize_sse2_t<simd_loadu_epi128, simd_storeu_epi128, binarize_255_x_sse2_op, binarize_255_x>;
+Processor *binarize_255_t_sse2 = &binarize_sse2_t<simd_loadu_epi128, simd_storeu_epi128, binarize_255_t_sse2_op, binarize_255_t>;
+
+Processor *upper_asse2 = &binarize_sse2_t<simd_load_epi128, simd_store_epi128, upper_sse2_op, upper>;
+Processor *lower_asse2 = &binarize_sse2_t<simd_load_epi128, simd_store_epi128, lower_sse2_op, lower>;
+Processor *binarize_0_x_asse2 = &binarize_sse2_t<simd_load_epi128, simd_store_epi128, binarize_0_x_sse2_op, binarize_0_x>;
+Processor *binarize_t_x_asse2 = &binarize_sse2_t<simd_load_epi128, simd_store_epi128, binarize_t_x_sse2_op, binarize_t_x>;
+Processor *binarize_x_0_asse2 = &binarize_sse2_t<simd_load_epi128, simd_store_epi128, binarize_x_0_sse2_op, binarize_x_0>;
+Processor *binarize_x_t_asse2 = &binarize_sse2_t<simd_load_epi128, simd_store_epi128, binarize_x_t_sse2_op, binarize_x_t>;
+Processor *binarize_t_0_asse2 = &binarize_sse2_t<simd_load_epi128, simd_store_epi128, binarize_t_0_sse2_op, binarize_t_0>;
+Processor *binarize_0_t_asse2 = &binarize_sse2_t<simd_load_epi128, simd_store_epi128, binarize_0_t_sse2_op, binarize_0_t>;
+Processor *binarize_x_255_asse2 = &binarize_sse2_t<simd_load_epi128, simd_store_epi128, binarize_x_255_sse2_op, binarize_x_255>;
+Processor *binarize_t_255_asse2 = &binarize_sse2_t<simd_load_epi128, simd_store_epi128, binarize_t_255_sse2_op, binarize_t_255>;
+Processor *binarize_255_x_asse2 = &binarize_sse2_t<simd_load_epi128, simd_store_epi128, binarize_255_x_sse2_op, binarize_255_x>;
+Processor *binarize_255_t_asse2 = &binarize_sse2_t<simd_load_epi128, simd_store_epi128, binarize_255_t_sse2_op, binarize_255_t>;
+
+
 
 } } } }
--- masktools/filters/binarize/binarize.h
+++ masktools/filters/binarize/binarize.h
@@ -7,24 +7,17 @@ namespace Filtering { namespace MaskTools { namespace Filters { namespace Binari
 
 typedef void(Processor)(Byte *pDst, ptrdiff_t nDstPitch, Byte nThreshold, int nWidth, int nHeight);
 
-#define DEFINE_PROCESSOR_NO_MMX(name) \
-   extern Processor *name##_c; \
-   extern "C" Processor Binarize_##name##8_mmx; \
-   extern "C" Processor Binarize_##name##8_isse; \
-   extern "C" Processor Binarize_##name##8_3dnow; \
-   extern "C" Processor Binarize_##name##8_sse2; \
-   extern "C" Processor Binarize_##name##8_asse2
-
 #define DEFINE_PROCESSOR(name) \
-   DEFINE_PROCESSOR_NO_MMX(name); \
-   extern "C" Processor Binarize_##name##8_mmx
+   extern Processor *name##_c; \
+   extern Processor *name##_sse2; \
+   extern Processor *name##_asse2
 
 DEFINE_PROCESSOR(upper);
 DEFINE_PROCESSOR(lower);
 DEFINE_PROCESSOR(binarize_0_x);
-DEFINE_PROCESSOR_NO_MMX(binarize_t_x);
+DEFINE_PROCESSOR(binarize_t_x);
 DEFINE_PROCESSOR(binarize_x_0);
-DEFINE_PROCESSOR_NO_MMX(binarize_x_t);
+DEFINE_PROCESSOR(binarize_x_t);
 DEFINE_PROCESSOR(binarize_t_0);
 DEFINE_PROCESSOR(binarize_0_t);
 DEFINE_PROCESSOR(binarize_x_255);
@@ -32,7 +25,6 @@ DEFINE_PROCESSOR(binarize_t_255);
 DEFINE_PROCESSOR(binarize_255_x);
 DEFINE_PROCESSOR(binarize_255_t);
 
-#undef DEFINE_PROCESSOR_NO_MMX
 #undef DEFINE_PROCESSOR
 
 class Binarize : public MaskTools::Filter
@@ -52,27 +44,20 @@ public:
    {
       nThreshold = convert<Byte, int>( parameters["threshold"].toInt() );
 
-#define SET_MODE_NO_MMX(mode) \
-   do { \
-      processors.push_back( Filtering::Processor<Processor>( mode##_c, Constraint( CPU_NONE, 1, 1, 1, 1 ), 0 ) ); \
-      processors.push_back( Filtering::Processor<Processor>( Binarize_##mode##8_isse, Constraint( CPU_ISSE , 8, 1, 1, 1 ), 2 ) ); \
-      processors.push_back( Filtering::Processor<Processor>( Binarize_##mode##8_3dnow, Constraint( CPU_3DNOW , 8, 1, 1, 1 ), 3 ) ); \
-      processors.push_back( Filtering::Processor<Processor>( Binarize_##mode##8_sse2, Constraint( CPU_SSE2 , 8, 1, 1, 1 ), 4 ) ); \
-      processors.push_back( Filtering::Processor<Processor>( Binarize_##mode##8_asse2, Constraint( CPU_SSE2 , 8, 1, 16, 16 ), 5 ) ); \
-   } while(0)
-
 #define SET_MODE(mode) \
    do { \
-      SET_MODE_NO_MMX(mode); \
-      processors.push_back( Filtering::Processor<Processor>( Binarize_##mode##8_mmx, Constraint( CPU_MMX , 8, 1, 1, 1 ), 2 ) ); \
+      processors.push_back( Filtering::Processor<Processor>( mode##_c, Constraint( CPU_NONE, 1, 1, 1, 1 ), 0 ) ); \
+      processors.push_back( Filtering::Processor<Processor>( mode##_sse2, Constraint( CPU_SSE2 , 1, 1, 1, 1 ), 1 ) ); \
+      processors.push_back( Filtering::Processor<Processor>( mode##_asse2, Constraint( CPU_SSE2 , 1, 1, 16, 16 ), 2 ) ); \
    } while(0)
+
 
 #define IF_(mode, a, b) if (parameters["mode"].is_defined() && parameters["mode"].toString() == #a" "#b) { mode(binarize_##a##_##b); }
 
       IF_(SET_MODE, 0, x)
-      else IF_(SET_MODE_NO_MMX, t, x)
+      else IF_(SET_MODE, t, x)
       else IF_(SET_MODE, x, 0)
-      else IF_(SET_MODE_NO_MMX, x, t)
+      else IF_(SET_MODE, x, t)
       else IF_(SET_MODE, t, 0)
       else IF_(SET_MODE, 0, t)
       else IF_(SET_MODE, x, 255)
@@ -85,9 +70,7 @@ public:
          SET_MODE(lower);
 
 #undef IF_SET_MODE
-#undef SET_MODE_NO_MMX
 #undef SET_MODE
-
    }
 
    InputConfiguration &input_configuration() const { return InPlaceOneFrame(); }
