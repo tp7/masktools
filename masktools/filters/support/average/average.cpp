39546fa7ca9be3dbe9271434fc6cb31e36363078
tp7 <tp7ptr@gmail.com>
2013-07-06 11:13:44 +1100

    Rewrite mt_average a bit
    Rewrite unrolling and replace mmx version with sse2. Required for x64.

--- masktools/filters/support/average/average.cpp
+++ masktools/filters/support/average/average.cpp
@@ -5,28 +5,27 @@ namespace Filtering { namespace MaskTools { namespace Filters { namespace Suppor
 
 void average_c(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nWidth, int nHeight)
 {
-   for ( int y = 0; y < nHeight; y++, pDst += nDstPitch, pSrc += nSrcPitch )
-      for ( int x = 0; x < nWidth; x++ )
-         pDst[x] = (int(pDst[x]) + pSrc[x] + 1) >> 1;
+    for ( int y = 0; y < nHeight; y++, pDst += nDstPitch, pSrc += nSrcPitch )
+        for ( int x = 0; x < nWidth; x++ )
+            pDst[x] = (int(pDst[x]) + pSrc[x] + 1) >> 1;
 }
 
-static inline void average_mmx(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nWidth, int nHeight)
+template<decltype(simd_load_epi128) load, decltype(simd_store_epi128) store>
+static FORCEINLINE void process_block_average_sse2(Byte *pDst, const Byte *pSrc)
 {
-    for ( int j = 0; j < nHeight; ++j ) {
-        for ( int i = 0; i < nWidth; i+=8 ) {
-            *reinterpret_cast<__m64*>(pDst+i) = _m_pavgb(*reinterpret_cast<const __m64*>(pDst + i), *reinterpret_cast<const __m64*>(pSrc + i));
-        }
-        pDst += nDstPitch;
-        pSrc += nSrcPitch;
-    }
-    _m_empty();
+    auto dst  = load(reinterpret_cast<const __m128i*>(pDst));
+    auto src  = load(reinterpret_cast<const __m128i*>(pSrc));
+
+    auto result  = _mm_avg_epu8(dst, src);
+
+    store(reinterpret_cast<__m128i*>(pDst), result);
 }
 
 template<decltype(simd_load_epi128) load, decltype(simd_store_epi128) store>
 static void average_sse2_t(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, ptrdiff_t nSrcPitch, int nWidth, int nHeight)
 {
     int wMod64 = (nWidth / 64) * 64;
-    int wMod8 = (nWidth / 8) * 8;
+    int wMod16 = (nWidth / 16) * 16;
     auto pDst2 = pDst;
     auto pSrc2 = pSrc;
 
@@ -35,34 +34,29 @@ static void average_sse2_t(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc, pt
             _mm_prefetch(reinterpret_cast<const char*>(pDst)+i+256, _MM_HINT_T0);
             _mm_prefetch(reinterpret_cast<const char*>(pSrc)+i+256, _MM_HINT_T0);
 
-            auto dst  = load(reinterpret_cast<const __m128i*>(pDst+i));
-            auto dst2 = load(reinterpret_cast<const __m128i*>(pDst+i+16));
-            auto dst3 = load(reinterpret_cast<const __m128i*>(pDst+i+32));
-            auto dst4 = load(reinterpret_cast<const __m128i*>(pDst+i+48));
-            auto src  = load(reinterpret_cast<const __m128i*>(pSrc+i));
-            auto src2 = load(reinterpret_cast<const __m128i*>(pSrc+i+16));
-            auto src3 = load(reinterpret_cast<const __m128i*>(pSrc+i+32));
-            auto src4 = load(reinterpret_cast<const __m128i*>(pSrc+i+48));
-
-            auto result  = _mm_avg_epu8(dst, src);
-            auto result2 = _mm_avg_epu8(dst2, src2);
-            auto result3 = _mm_avg_epu8(dst3, src3);
-            auto result4 = _mm_avg_epu8(dst4, src4);
-
-            store(reinterpret_cast<__m128i*>(pDst+i), result);
-            store(reinterpret_cast<__m128i*>(pDst+i+16), result2);
-            store(reinterpret_cast<__m128i*>(pDst+i+32), result3);
-            store(reinterpret_cast<__m128i*>(pDst+i+48), result4);
+            process_block_average_sse2<load,store>(pDst+i, pSrc+i);
+            process_block_average_sse2<load,store>(pDst+i+16, pSrc+i+16);
+            process_block_average_sse2<load,store>(pDst+i+32, pSrc+i+32);
+            process_block_average_sse2<load,store>(pDst+i+48, pSrc+i+48);
         }
         pDst += nDstPitch;
         pSrc += nSrcPitch;
     }
 
-    if (nWidth > wMod64) {
-        average_mmx(pDst2 + wMod64, nDstPitch, pSrc2 + wMod64, nSrcPitch, wMod8 - wMod64, nHeight);
+    if (nWidth > wMod16) {
+        pDst = pDst2 + wMod64;
+        pSrc = pSrc2 + wMod64;
+
+        for ( int j = 0; j < nHeight; ++j ) {
+            for ( int i = 0; i < wMod16 - wMod64; i+=16 ) {
+                 process_block_average_sse2<load,store>(pDst+i, pSrc+i);
+            }
+            pDst += nDstPitch;
+            pSrc += nSrcPitch;
+        }
     }
-    if (nWidth > wMod8) {
-        average_c(pDst2 + wMod8, nDstPitch, pSrc2 + wMod8, nSrcPitch, nWidth - wMod8, nHeight);
+    if (nWidth > wMod16) {
+        average_c(pDst2 + wMod16, nDstPitch, pSrc2 + wMod16, nSrcPitch, nWidth - wMod16, nHeight);
     }
 }
 
