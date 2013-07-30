6aac86b7a2a61d0aee4c87c1c144710ac13c337a
tp7 <tp7ptr@gmail.com>
2013-07-30 22:53:00 +1100

    Actually use functions::copy_c in mt_lutspa instead of just copying its code

--- masktools/filters/lut/lutspa/lutspa.cpp
+++ masktools/filters/lut/lutspa/lutspa.cpp
@@ -4,14 +4,5 @@ using namespace Filtering;
 
 void Filtering::MaskTools::Filters::Lut::Coordinate::lut_c(Byte *pDst, ptrdiff_t nDstPitch, int nWidth, int nHeight, const Byte lut[65536])
 {
-    if (nDstPitch == nWidth) {
-        memcpy(pDst, lut, nWidth*nHeight);
-    } else {
-        for (int y = 0; y < nHeight; ++y) {
-            memcpy(pDst, lut, nWidth);
-            pDst += nDstPitch;
-            lut += nWidth;
-        }
-    }
+    Functions::copy_c(pDst, nDstPitch, lut, nWidth, nWidth, nHeight);
 }
-
