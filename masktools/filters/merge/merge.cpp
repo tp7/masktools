d468a691c96f4ac403e9c9c1acba09cfbab5a722
tp7 <tp7ptr@gmail.com>
2013-07-30 22:45:49 +1100

    Rename pSrc2 -> pMask in mt_merge

--- masktools/filters/merge/merge.cpp
+++ masktools/filters/merge/merge.cpp
@@ -4,31 +4,31 @@
 namespace Filtering { namespace MaskTools { namespace Filters { namespace Merge {
 
 void merge_c(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc1, ptrdiff_t nSrc1Pitch,
-                                                   const Byte *pSrc2, ptrdiff_t nSrc2Pitch, int nWidth, int nHeight)
+                                                   const Byte *pMask, ptrdiff_t nSrc2Pitch, int nWidth, int nHeight)
 {
    for ( int y = 0; y < nHeight; ++y )
    {
       for ( int x = 0; x < nWidth; ++x )
-         pDst[x] = ((256 - int(pSrc2[x])) * pDst[x] + int(pSrc2[x]) * pSrc1[x] + 128) >> 8;
+         pDst[x] = ((256 - int(pMask[x])) * pDst[x] + int(pMask[x]) * pSrc1[x] + 128) >> 8;
       pDst += nDstPitch;
       pSrc1 += nSrc1Pitch;
-      pSrc2 += nSrc2Pitch;
+      pMask += nSrc2Pitch;
    }
 }
 
 void merge_luma_420_c(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc1, ptrdiff_t nSrc1Pitch,
-                                                        const Byte *pSrc2, ptrdiff_t nSrc2Pitch, int nWidth, int nHeight)
+                                                        const Byte *pMask, ptrdiff_t nSrc2Pitch, int nWidth, int nHeight)
 {
    for ( int y = 0; y < nHeight; ++y )
    {
       for ( int x = 0; x < nWidth; ++x )
       {
-         const int nMask = (((pSrc2[x*2] + pSrc2[x*2+nSrc2Pitch] + 1) >> 1) + ((pSrc2[x*2+1] + pSrc2[x*2+nSrc2Pitch+1] + 1) >> 1) + 1) >> 1;
+         const int nMask = (((pMask[x*2] + pMask[x*2+nSrc2Pitch] + 1) >> 1) + ((pMask[x*2+1] + pMask[x*2+nSrc2Pitch+1] + 1) >> 1) + 1) >> 1;
          pDst[x] = static_cast<Byte>(((256 - int(nMask)) * pDst[x] + int(nMask) * pSrc1[x] + 128) >> 8);
       }
       pDst += nDstPitch;
       pSrc1 += nSrc1Pitch;
-      pSrc2 += nSrc2Pitch * 2;
+      pMask += nSrc2Pitch * 2;
    }
 }
 
@@ -64,19 +64,19 @@ MT_FORCEINLINE __m128i merge_sse2_core(Byte *pDst, const Byte *pSrc, const __m12
 
 template <decltype(simd_store_epi128) store>
 void merge_sse2_t(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc1, ptrdiff_t nSrc1Pitch,
-                                                   const Byte *pSrc2, ptrdiff_t nSrc2Pitch, int nWidth, int nHeight)
+                                                   const Byte *pMask, ptrdiff_t nSrc2Pitch, int nWidth, int nHeight)
 {
     int wMod16 = (nWidth / 16) * 16;
     auto pDst_s = pDst;
     auto pSrc1_s = pSrc1;
-    auto pSrc2_s = pSrc2;
+    auto pMask_s = pMask;
     auto v256 = _mm_set1_epi16(0x0100);
     auto v128 = _mm_set1_epi16(0x0080);
     auto zero = _mm_setzero_si128();
     for ( int j = 0; j < nHeight; ++j ) {
         for ( int i = 0; i < wMod16; i+=16 ) {
-            auto src2_t1 = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(pSrc2+i));
-            auto src2_t2 = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(pSrc2+8+i));
+            auto src2_t1 = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(pMask+i));
+            auto src2_t2 = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(pMask+8+i));
             auto mask_t1 = _mm_unpacklo_epi8(src2_t1, zero);
             auto mask_t2 = _mm_unpacklo_epi8(src2_t2, zero);
 
@@ -86,35 +86,35 @@ void merge_sse2_t(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc1, ptrdiff_t 
         }
         pDst += nDstPitch;
         pSrc1 += nSrc1Pitch;
-        pSrc2 += nSrc2Pitch;
+        pMask += nSrc2Pitch;
     }
 
     if (nWidth > wMod16) {
-        merge_c(pDst_s + wMod16, nDstPitch, pSrc1_s + wMod16, nSrc1Pitch, pSrc2_s + wMod16, nSrc2Pitch, nWidth-wMod16, nHeight);
+        merge_c(pDst_s + wMod16, nDstPitch, pSrc1_s + wMod16, nSrc1Pitch, pMask_s + wMod16, nSrc2Pitch, nWidth-wMod16, nHeight);
     }
 }
 
 template <decltype(simd_load_epi128) load, decltype(simd_store_epi128) store>
 void merge_luma_420_sse2_t(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc1, ptrdiff_t nSrc1Pitch,
-                  const Byte *pSrc2, ptrdiff_t nSrc2Pitch, int nWidth, int nHeight)
+                  const Byte *pMask, ptrdiff_t nSrc2Pitch, int nWidth, int nHeight)
 {
     int wMod16 = (nWidth / 16) * 16;
     auto pDst_s = pDst;
     auto pSrc1_s = pSrc1;
-    auto pSrc2_s = pSrc2;
+    auto mPask_s = pMask;
     auto v256 = _mm_set1_epi16(0x0100);
     auto v255 = _mm_set1_epi16(0x00FF);
     auto v128 = _mm_set1_epi16(0x0080);
     auto zero = _mm_setzero_si128();
     for ( int j = 0; j < nHeight; ++j ) {
         for ( int i = 0; i < wMod16; i+=16 ) {
-            _mm_prefetch(reinterpret_cast<const char*>(pSrc2)+ i*2 + 64, _MM_HINT_T0);
-            _mm_prefetch(reinterpret_cast<const char*>(pSrc2)+ nSrc2Pitch + i*2 + 64, _MM_HINT_T0);
+            _mm_prefetch(reinterpret_cast<const char*>(pMask)+ i*2 + 64, _MM_HINT_T0);
+            _mm_prefetch(reinterpret_cast<const char*>(pMask)+ nSrc2Pitch + i*2 + 64, _MM_HINT_T0);
             // preparing mask
-            auto src2_row1_t1 = load(reinterpret_cast<const __m128i*>(pSrc2 + i*2));
-            auto src2_row1_t2 = load(reinterpret_cast<const __m128i*>(pSrc2 + i*2 + 16));
-            auto src2_row2_t1 = load(reinterpret_cast<const __m128i*>(pSrc2 + nSrc2Pitch + i*2));
-            auto src2_row2_t2 = load(reinterpret_cast<const __m128i*>(pSrc2 + nSrc2Pitch + i*2 + 16));
+            auto src2_row1_t1 = load(reinterpret_cast<const __m128i*>(pMask + i*2));
+            auto src2_row1_t2 = load(reinterpret_cast<const __m128i*>(pMask + i*2 + 16));
+            auto src2_row2_t1 = load(reinterpret_cast<const __m128i*>(pMask + nSrc2Pitch + i*2));
+            auto src2_row2_t2 = load(reinterpret_cast<const __m128i*>(pMask + nSrc2Pitch + i*2 + 16));
             auto avg_t1 = _mm_avg_epu8(src2_row1_t1,src2_row2_t1);
             auto avg_t2 = _mm_avg_epu8(src2_row1_t2,src2_row2_t2);
             auto shifted_t1 = _mm_srli_si128(avg_t1, 1);
@@ -130,10 +130,10 @@ void merge_luma_420_sse2_t(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc1, p
         }
         pDst += nDstPitch;
         pSrc1 += nSrc1Pitch;
-        pSrc2 += nSrc2Pitch * 2;
+        pMask += nSrc2Pitch * 2;
     }
     if (nWidth > wMod16) {
-        merge_c(pDst_s + wMod16, nDstPitch, pSrc1_s + wMod16, nSrc1Pitch, pSrc2_s + wMod16*2, nSrc2Pitch, nWidth-wMod16, nHeight);
+        merge_c(pDst_s + wMod16, nDstPitch, pSrc1_s + wMod16, nSrc1Pitch, mPask_s + wMod16*2, nSrc2Pitch, nWidth-wMod16, nHeight);
     }
 }
 
