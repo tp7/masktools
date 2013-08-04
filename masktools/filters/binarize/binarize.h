e37661d8a9b70a2d81a02738709d1c9687a57b78
tp7 <tp7ptr@gmail.com>
2013-08-04 16:34:13 +1100

    Simplify preprocessort magic in mt_binarize a bit

--- masktools/filters/binarize/binarize.h
+++ masktools/filters/binarize/binarize.h
@@ -8,22 +8,22 @@ namespace Filtering { namespace MaskTools { namespace Filters { namespace Binari
 typedef void(Processor)(Byte *pDst, ptrdiff_t nDstPitch, Byte nThreshold, int nWidth, int nHeight);
 
 #define DEFINE_PROCESSOR(name) \
-   extern Processor *name##_c; \
-   extern Processor *name##_sse2; \
-   extern Processor *name##_asse2
+   extern Processor *binarize_##name##_c; \
+   extern Processor *binarize_##name##_sse2; \
+   extern Processor *binarize_##name##_asse2
 
 DEFINE_PROCESSOR(upper);
 DEFINE_PROCESSOR(lower);
-DEFINE_PROCESSOR(binarize_0_x);
-DEFINE_PROCESSOR(binarize_t_x);
-DEFINE_PROCESSOR(binarize_x_0);
-DEFINE_PROCESSOR(binarize_x_t);
-DEFINE_PROCESSOR(binarize_t_0);
-DEFINE_PROCESSOR(binarize_0_t);
-DEFINE_PROCESSOR(binarize_x_255);
-DEFINE_PROCESSOR(binarize_t_255);
-DEFINE_PROCESSOR(binarize_255_x);
-DEFINE_PROCESSOR(binarize_255_t);
+DEFINE_PROCESSOR(0_x);
+DEFINE_PROCESSOR(t_x);
+DEFINE_PROCESSOR(x_0);
+DEFINE_PROCESSOR(x_t);
+DEFINE_PROCESSOR(t_0);
+DEFINE_PROCESSOR(0_t);
+DEFINE_PROCESSOR(x_255);
+DEFINE_PROCESSOR(t_255);
+DEFINE_PROCESSOR(255_x);
+DEFINE_PROCESSOR(255_t);
 
 #undef DEFINE_PROCESSOR
 
@@ -39,37 +39,36 @@ protected:
       processors.best_processor( constraints[nPlane] )( dst, dst.pitch(), nThreshold, dst.width(), dst.height() );
    }
 
+   bool isMode(const char *mode) {
+       return parameters["mode"].is_defined() && parameters["mode"].toString() == mode;
+   }
+
 public:
    Binarize(const Parameters &parameters) : MaskTools::Filter(parameters, FilterProcessingType::INPLACE)
    {
       nThreshold = convert<Byte, int>( parameters["threshold"].toInt() );
 
 #define SET_MODE(mode) \
-   do { \
-      processors.push_back( Filtering::Processor<Processor>( mode##_c, Constraint( CPU_NONE, 1, 1, 1, 1 ), 0 ) ); \
-      processors.push_back( Filtering::Processor<Processor>( mode##_sse2, Constraint( CPU_SSE2 , 1, 1, 1, 1 ), 1 ) ); \
-      processors.push_back( Filtering::Processor<Processor>( mode##_asse2, Constraint( CPU_SSE2 , 1, 1, 16, 16 ), 2 ) ); \
-   } while(0)
+      processors.push_back( Filtering::Processor<Processor>( binarize_##mode##_c, Constraint( CPU_NONE, 1, 1, 1, 1 ), 0 ) ); \
+      processors.push_back( Filtering::Processor<Processor>( binarize_##mode##_sse2, Constraint( CPU_SSE2 , 1, 1, 1, 1 ), 1 ) ); \
+      processors.push_back( Filtering::Processor<Processor>( binarize_##mode##_asse2, Constraint( CPU_SSE2 , 1, 1, 16, 16 ), 2 ) ); 
 
-
-#define IF_(mode, a, b) if (parameters["mode"].is_defined() && parameters["mode"].toString() == #a" "#b) { mode(binarize_##a##_##b); }
-
-      IF_(SET_MODE, 0, x)
-      else IF_(SET_MODE, t, x)
-      else IF_(SET_MODE, x, 0)
-      else IF_(SET_MODE, x, t)
-      else IF_(SET_MODE, t, 0)
-      else IF_(SET_MODE, 0, t)
-      else IF_(SET_MODE, x, 255)
-      else IF_(SET_MODE, t, 255)
-      else IF_(SET_MODE, 255, x)
-      else IF_(SET_MODE, 255, t)
-      else if ((parameters["mode"].is_defined() && (parameters["mode"].toString() == "upper" || parameters["mode"].toString() == "0 255")) || (!parameters["mode"].is_defined() && parameters["upper"].toBool()))
+      if (isMode("0 x")) { SET_MODE(0_x); }
+      else if (isMode("t x")) { SET_MODE(t_x); }
+      else if (isMode("x 0")) { SET_MODE(x_0); }
+      else if (isMode("x t")) { SET_MODE(x_t); }
+      else if (isMode("t 0")) { SET_MODE(t_0); }
+      else if (isMode("0 t")) { SET_MODE(0_t); }
+      else if (isMode("x 255")) { SET_MODE(x_255); }
+      else if (isMode("t 255")) { SET_MODE(t_255); }
+      else if (isMode("255 x")) { SET_MODE(255_x); }
+      else if (isMode("255 t")) { SET_MODE(255_t); }
+      else if ((parameters["mode"].is_defined() && (parameters["mode"].toString() == "upper" || parameters["mode"].toString() == "0 255")) || (!parameters["mode"].is_defined() && parameters["upper"].toBool())) {
          SET_MODE(upper);
-      else
+      } else {
          SET_MODE(lower);
+      }
 
-#undef IF_SET_MODE
 #undef SET_MODE
    }
 
