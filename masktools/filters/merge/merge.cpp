968f8d8d4e4b8b087dba9499979cc5e426f1a065
Victor Efimov <tp7ptr@gmail.com>
2013-11-30 13:11:00 +1100

    Optimize mt_merge a bit
    Code provided by innocenat

--- masktools/filters/merge/merge.cpp
+++ masktools/filters/merge/merge.cpp
@@ -34,31 +34,30 @@ void merge_luma_420_c(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc1, ptrdif
 
 template <MemoryMode mem_mode>
 MT_FORCEINLINE __m128i merge_sse2_core(Byte *pDst, const Byte *pSrc, const __m128i& mask_lo, const __m128i& mask_hi, 
-                                       const __m128i& v128, const __m128i& v256, const __m128i& zero) {
+                                       const __m128i& v128, const __m128i& zero) {
     auto dst = simd_load_si128<mem_mode>(pDst);
-    auto unpacked_dst_t1 = _mm_unpacklo_epi8(dst, zero);
-    auto unpacked_dst_t2 = _mm_unpackhi_epi8(dst, zero);
+    auto dst_lo = _mm_unpacklo_epi8(dst, zero);
+    auto dst_hi = _mm_unpackhi_epi8(dst, zero);
 
     auto src = simd_load_si128<mem_mode>(pSrc);
-    auto unpacked_src1_t1 = _mm_unpacklo_epi8(src, zero);
-    auto unpacked_src1_t2 = _mm_unpackhi_epi8(src, zero);
+    auto src1_lo = _mm_unpacklo_epi8(src, zero);
+    auto src1_hi = _mm_unpackhi_epi8(src, zero);
 
-    auto temp1_t1 = _mm_mullo_epi16(_mm_sub_epi16(v256, mask_lo), unpacked_dst_t1); //(256 - pSrc2[x]) * pDst[x]
-    auto temp1_t2 = _mm_mullo_epi16(_mm_sub_epi16(v256, mask_hi), unpacked_dst_t2);
+    auto dst_lo_sh = _mm_slli_epi16(dst_lo, 8);
+    auto diff_lo = _mm_sub_epi16(src1_lo, dst_lo);
+    auto tmp1_lo = _mm_mullo_epi16(diff_lo, mask_lo); // (p2-p1)*mask
+    auto tmp2_lo = _mm_or_si128(dst_lo_sh, v128);    // p1<<8 + 128 == p1<<8 | 128
+    auto result_lo = _mm_add_epi16(tmp1_lo, tmp2_lo);
+    result_lo = _mm_srli_epi16(result_lo, 8);
 
-    auto temp2_t1 = _mm_mullo_epi16(mask_lo, unpacked_src1_t1); // pSrc2[x] * pSrc1[x]
-    auto temp2_t2 = _mm_mullo_epi16(mask_hi, unpacked_src1_t2);
+    auto dst_hi_sh = _mm_slli_epi16(dst_hi, 8);
+    auto diff_hi = _mm_sub_epi16(src1_hi, dst_hi);
+    auto tmp1_hi = _mm_mullo_epi16(diff_hi, mask_hi); // (p2-p1)*mask
+    auto tmp2_hi = _mm_or_si128(dst_hi_sh, v128);    // p1<<8 + 128 == p1<<8 | 128
+    auto result_hi = _mm_add_epi16(tmp1_hi, tmp2_hi);
+    result_hi = _mm_srli_epi16(result_hi, 8);
 
-    temp1_t1 = _mm_add_epi16(temp1_t1, temp2_t1);
-    temp1_t2 = _mm_add_epi16(temp1_t2, temp2_t2);
-
-    temp1_t1 = _mm_add_epi16(temp1_t1, v128);
-    temp1_t2 = _mm_add_epi16(temp1_t2, v128);
-
-    auto result1 = _mm_srli_epi16(temp1_t1, 8);
-    auto result2 = _mm_srli_epi16(temp1_t2, 8);
-
-    return _mm_packus_epi16(result1, result2);
+    return _mm_packus_epi16(result_lo, result_hi);
 }
 
 template <MemoryMode mem_mode>
@@ -69,7 +68,6 @@ void merge_sse2_t(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc1, ptrdiff_t 
     auto pDst_s = pDst;
     auto pSrc1_s = pSrc1;
     auto pMask_s = pMask;
-    auto v256 = _mm_set1_epi16(0x0100);
     auto v128 = _mm_set1_epi16(0x0080);
     auto zero = _mm_setzero_si128();
     for ( int j = 0; j < nHeight; ++j ) {
@@ -78,7 +76,7 @@ void merge_sse2_t(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc1, ptrdiff_t 
             auto mask_t1 = _mm_unpacklo_epi8(src2, zero);
             auto mask_t2 = _mm_unpackhi_epi8(src2, zero);
 
-            auto result = merge_sse2_core<mem_mode>(pDst+i, pSrc1+i, mask_t1, mask_t2, v128, v256, zero);
+            auto result = merge_sse2_core<mem_mode>(pDst+i, pSrc1+i, mask_t1, mask_t2, v128, zero);
 
             simd_store_si128<mem_mode>(pDst+i, result);
         }
@@ -100,7 +98,6 @@ void merge_luma_420_sse2_t(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc1, p
     auto pDst_s = pDst;
     auto pSrc1_s = pSrc1;
     auto mPask_s = pMask;
-    auto v256 = _mm_set1_epi16(0x0100);
     auto v255 = _mm_set1_epi16(0x00FF);
     auto v128 = _mm_set1_epi16(0x0080);
     auto zero = _mm_setzero_si128();
@@ -122,7 +119,7 @@ void merge_luma_420_sse2_t(Byte *pDst, ptrdiff_t nDstPitch, const Byte *pSrc1, p
             auto mask_t1 = _mm_and_si128(avg_t1, v255);
             auto mask_t2 = _mm_and_si128(avg_t2, v255);
 
-            auto result = merge_sse2_core<mem_mode>(pDst+i, pSrc1+i, mask_t1, mask_t2, v128, v256, zero);
+            auto result = merge_sse2_core<mem_mode>(pDst+i, pSrc1+i, mask_t1, mask_t2, v128, zero);
           
             simd_store_si128<mem_mode>(pDst+i, result);
         }
