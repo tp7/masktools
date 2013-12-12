049024e7d819733d472eb9ea22a42c463fe59ee7
Victor Efimov <tp7ptr@gmail.com>
2013-12-12 20:35:08 +1100

    Fix some project file stuff

--- masktools/common/mt_resource.h
+++ masktools/common/mt_resource.h
@@ -1,2 +1,15 @@
-#define MT_RESOURCE_FILE_VERSION 2.0 
-#define MT_RESOURCE_VERSION 2,0,48,0 
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by mt_resource.rc
+//
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
